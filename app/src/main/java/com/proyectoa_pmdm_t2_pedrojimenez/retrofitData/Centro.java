69f0eb8ad01e0ce4cfcd106d50a6996c4691d28f
Pedrojimvi <pedrojv1030@gmail.com>
2023-03-04 16:39:09 +0100

    Changed an error

--- app/src/main/java/com/proyectoa_pmdm_t2_pedrojimenez/retrofitData/Centro.java
+++ app/src/main/java/com/proyectoa_pmdm_t2_pedrojimenez/retrofitData/Centro.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class CentroRes {
+public class Centro {
     @SerializedName("@context")
     @Expose
     private Context context;
--- app/src/main/java/com/proyectoa_pmdm_t2_pedrojimenez/retrofitUtils/APIRestService.java
+++ app/src/main/java/com/proyectoa_pmdm_t2_pedrojimenez/retrofitUtils/APIRestService.java
@@ -1,6 +1,6 @@
 package com.proyectoa_pmdm_t2_pedrojimenez.retrofitUtils;
 
-import com.proyectoa_pmdm_t2_pedrojimenez.retrofitData.CentroRes;
+import com.proyectoa_pmdm_t2_pedrojimenez.retrofitData.Centro;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -11,13 +11,13 @@ public interface APIRestService {
     public static final String BASE_URL = "https://datos.madrid.es/egob/catalogo/";
 
     @GET("203166-0-universidades-educacion.json")
-    Call<CentroRes> getDataFilter(@Query("latitud") double lat,
-                                  @Query("longitud") double lon,
-                                  @Query("distancia") int dist);
+    Call<Centro> getDataFilter(@Query("latitud") double lat,
+                               @Query("longitud") double lon,
+                               @Query("distancia") int dist);
 
     @GET("203166-0-universidades-educacion.json")
-    Call<CentroRes> getData();
+    Call<Centro> getData();
 
     @GET("tipo/entidadesyorganismos/{id_url}")
-    Call<CentroRes> getData(@Path("id_url") String url);
+    Call<Centro> getData(@Path("id_url") String url);
 }
